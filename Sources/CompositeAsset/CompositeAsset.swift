6524245c20c6802c186c336a3dfb1a0183c0440d
Nicolás Miari <nicolasmiari@gmail.com>
2022-10-10 14:46:28 +0900

    Initial Commit

---
+++ Sources/CompositeAsset/CompositeAsset.swift
@@ -0,0 +1,6 @@
+public struct CompositeAsset {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CompositeAssetTests/CompositeAssetTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CompositeAsset
+
+final class CompositeAssetTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CompositeAsset().text, "Hello, World!")
+    }
+}
