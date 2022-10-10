c5d7071ac7ac8a26dc03fc3bf45c4012eb240110
Nicolás Miari <nicolasmiari@gmail.com>
2022-10-10 15:09:29 +0900

    First working version.

--- Package.swift
+++ Package.swift
@@ -4,25 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "CompositeAsset",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CompositeAsset",
-            targets: ["CompositeAsset"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CompositeAsset",
-            dependencies: []),
-        .testTarget(
-            name: "CompositeAssetTests",
-            dependencies: ["CompositeAsset"]),
-    ]
+  name: "CompositeAsset",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "CompositeAsset",
+      targets: ["CompositeAsset"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/nicolas-miari/Asset.git", from: "2.0.0"),
+    .package(url: "https://github.com/nicolas-miari/UniqueIdentifierProvider.git", from: "1.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "CompositeAsset",
+      dependencies: [
+        .product(name: "Asset", package: "Asset"),
+        .product(name: "UniqueIdentifierProvider", package: "UniqueIdentifierProvider"),
+      ]),
+    // No test target (package only defines protocols, contains no testable implementation).
+  ]
 )
--- Sources/CompositeAsset/CompositeAsset.swift
+++ Sources/CompositeAsset/CompositeAsset.swift
@@ -1,6 +1,30 @@
-public struct CompositeAsset {
-    public private(set) var text = "Hello, World!"
+import Asset
+import UniqueIdentifierProvider
 
-    public init() {
-    }
+/// Defines the interface common to all asset types that depend on other assets.
+public protocol CompositeAsset: Asset {
+
+  /// A type used to pass configuration options to the asset initializer
+  associatedtype Options: CompositeAssetOptions
+
+  /// An array of strings each representing theunique identifier of an asset that is required for
+  /// the functioning of this one.
+  @MainActor
+  var dependencies: [String] { get set }
+
+  /// Creates a new instance with the specified asset IDs as dependencies (children) and a
+  /// configuration.
+  @MainActor
+  init(dependencies: [String], identifierProvider: UniqueIdentifierProvider, options: Options) throws
 }
+
+// MARK: - Supporting Types
+
+@MainActor
+public protocol CompositeAssetOptions {
+
+  /// At the bare minimum, an asset options should include the user-specified asset name. concrete
+  /// Option types for more specific assets might include additional properties.
+  var name: String { get }
+}
+
